9dbb2eb296f2f8ae7df8c09023e7f256cf631fee
LuHG18 <luhgoldfarb@gmail.com>
2018-06-08 12:20:09 -0400

    Add files via upload

---
+++ challenges.py
@@ -0,0 +1,55 @@
+import math
+#challenge 1
+#radius = raw_input()
+#area = int(radius) ** 2 * math.pi
+#print(str(area))
+
+#challenge 2
+#radius = raw_input()
+#volume = int(radius) ** 3 * math.pi * 4/3
+#print(str(volume))
+
+#challenge 3
+#x = int(raw_input())
+#y = int(raw_input())
+#print(x ** 2 + 2 * x * y + y ** 2)
+
+#challenge 4
+#kph = int(raw_input())
+#mph = (kph * 0.621371)
+#print str(mph)
+
+#challenge 5
+import random
+
+#for x in range(1):
+    #eightBall = random.randint(1,4)
+    #if eightBall == 1:
+        #print("yes")
+    #if eightBall == 2:
+        #print("no")
+    #if eightBall == 3:
+        #print("maybe")
+
+#challenge 6
+#print("enter number")
+#number = int(raw_input())
+#secondNumber = number
+#if secondNumber * -1200 < number:
+    #print("Positive")
+#else:
+    #print("Negative")
+
+#challenge 7
+
+
+
+
+
+
+
+
+
+
+    
+        
